6e8d9d82094f91034202a418d59af956b15b921b
Daniel Guzman <iamguzmand@gmail.com>
2019-04-11 02:19:21 -0500

    agregadas clases en blanco para el API

---
+++ PlayTrackRest/Controllers/DispositivosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PlayTrackRest.Controllers
+{
+    /// <summary>
+    /// Clase que manejara la informacion correspondiente a los dispositivos
+    /// </summary>
+    public class DispositivosController : ApiController
+    {
+        public string Get()
+        {
+            string respuesta = "Hola mundo";
+            return respuesta;
+        }
+    }
+}
